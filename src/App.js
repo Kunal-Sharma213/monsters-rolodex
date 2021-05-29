63a6431a4851038a880b5999e250d55515e4ebd7
Kunal-Sharma213 <riturishi77@gmail.com>
2021-05-30 00:22:26 +0530

    adding files to github pages

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React , { Component } from 'react';
+
+import { CardList } from './components/card-list/card-list.component.jsx'
+
+import { SearchBox } from './components/search-box/search-box.component.jsx'
+
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters : [],   // initial state set to empty
+      searchField : ''
+    }
+    
+    //this.handleChange = this.handleChange.bind(this)   -> this is used without arrow fuction
+  }
+  componentDidMount() {    // it will be called when the rendering is done for the very first time
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({ monsters: users }))
+    .catch(error => console.log('SERVER DOWN'))
+    //.then(users => console.log(users))
+  }
+  // handleChange(e) {    here we will need to bind this explicitely
+  //   this.setState({ searchField: e.target.value})
+  // }
+  handleChange = e => {
+    this.setState({ searchField: e.target.value})
+  }
+  render() {        // render is just only required to tell what is to be rendered onto the broweser screen
+    const { monsters , searchField } = this.state
+    // this will return an array of filteredMonsters
+    const filteredMonsters = monsters.filter(monster => 
+      monster.name.toLowerCase().includes(searchField.toLowerCase()))
+    return (
+      <div className="App">
+        <h1>MONSTERS ROLODEX</h1>
+         <SearchBox 
+          placeholder = "Search Monsters!!"
+          handleChange = { e => {this.setState({searchField: e.target.value})}}/>
+        <CardList monsters = {filteredMonsters} />
+      </div>
+    )
+  }
+}
+
+export default App;
